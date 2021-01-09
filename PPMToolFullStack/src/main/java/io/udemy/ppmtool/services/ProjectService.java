32865157219f4969de7ccb2f5a3f01d662938cac
n0303321 <dhvani.mehta@libertymutual.com>
2021-01-09 15:51:48 -0500

    Handled custom exception by using @ControllerAdvice and ResponseEntityExceptionHandler

--- PPMToolFullStack/src/main/java/io/udemy/ppmtool/services/ProjectService.java
+++ PPMToolFullStack/src/main/java/io/udemy/ppmtool/services/ProjectService.java
@@ -1,6 +1,7 @@
 package io.udemy.ppmtool.services;
 
 import io.udemy.ppmtool.domain.Project;
+import io.udemy.ppmtool.exceptions.ProjectIdException;
 import io.udemy.ppmtool.repositories.ProjectRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -12,6 +13,12 @@ public class ProjectService {
     private ProjectRepository projectRepository;
 
     public Project saveOrUpdateProject(Project project) {
-        return projectRepository.save(project);
+        try {
+            project.setProjectIdentifier(project.getProjectIdentifier().toUpperCase());
+            return projectRepository.save(project);
+        }catch(Exception e) {
+            throw new ProjectIdException("Project ID '" + project.getProjectIdentifier().toUpperCase() + "' already exists");
+        }
+
     }
 }
